41574cd0e1c7874686cf1cf840bedad687d3f9e7
nramiscal <nramiscal@gmail.com>
2017-09-11 19:08:03 -0400

    added optional check for .equals(Java)

--- src/main/java/com/nramiscal/dojoSurvey/controllers/HomeController.java
+++ src/main/java/com/nramiscal/dojoSurvey/controllers/HomeController.java
@@ -16,18 +16,30 @@ public class HomeController {
 	
 	@RequestMapping(path="/process", method=RequestMethod.POST)
     public String login(HttpSession session, @RequestParam(value="name") String name, @RequestParam(value="location") String location, @RequestParam(value="language") String language, @RequestParam(value="comment") String comment) {
-		session.setAttribute("name", name);
-		session.setAttribute("location", location);
-		session.setAttribute("language", language);
-		session.setAttribute("comment", comment);
+
+			session.setAttribute("name", name);
+			session.setAttribute("location", location);
+			session.setAttribute("language", language);
+			session.setAttribute("comment", comment);
+			return "redirect:myresults";
+		}
 		
-		return "redirect:myresults";
-	}
+
 	
 	@RequestMapping("myresults")
 	public String results(HttpSession session) {
-		return "results";
+		 if (session.getAttribute("language").equals("Java")) {
+			 return "java";
+		 }
+		 else {
+			 return "results";
+		 }
+		 
+		
+	
 	}
+	
+
 
 
 }
